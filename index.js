4a1aa66ff1ae97a79dca4e472371937f21135f79
taylornorris <taylornorrisdev@gmail.com>
2024-07-17 08:54:58 -0600

    Problem solving
    Wrote a bunch of code that didn't work. Trying to make the remove button work from the order section. Will probably require a rework.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+// IM STUCK REMOVING ITEMS FROM ORDER
+// CREATE NEW OBJECTS TO RENDER ORDER, USING OBJECT INDEX LINKED TO REMOVE BUTTON?
+// ALSO NEED TO CONSIDER REMOVING PRICE FROM PRICE ARRAY. MAYBE SUMMING NEW OBJECT KEY:VALUES INSTEAD OF STORING IN VARIABLE?
+
+
 import { menuArray } from "./data.js"
 
 let orderStarted = false
 let orderPriceArray = []
 let orderTotal = 0
+
+// RENDER MENU HTML
+document.getElementById("menu-section").innerHTML = getMenuHtml(menuArray)
 
 function getMenuHtml(menuArr) {
     return menuArr.map(item => {
@@ -12,7 +20,7 @@ function getMenuHtml(menuArr) {
             <div class="menu-item-desc">
                 <h2 class="menu-item-name">${item.name}</h2>
                 <p class="menu-item-ingredients">${item.ingredients}</p>
-                <p class="menu-item-price">${item.price}</p>
+                <p class="menu-item-price">$${item.price}</p>
             </div>
             <button class="menu-item-add-btn" data-add-item="${item.id}">+</button>
         </div>
@@ -21,8 +29,7 @@ function getMenuHtml(menuArr) {
     }).join("")
 }
 
-document.getElementById("menu-section").innerHTML = getMenuHtml(menuArray)
-
+// LISTEN FOR CLICKS AND CALL FUNCTIONS
 document.addEventListener("click", function(e) {
     if(e.target.dataset.addItem) {
         addMenuItemToCheckout(e.target.dataset.addItem)
@@ -35,10 +42,11 @@ function addMenuItemToCheckout(itemId) {
 
 function renderCheckoutItemHtml(item) {
     renderCheckoutTotalPrice(item)
+    
     return `
-    <div id="checkout-item-${item.id}"class="checkout-item-div">
+    <div id="checkout-item" class="checkout-item-div">
     <p class="checkout-item-name">${item.name}</p>
-    <button class="checkout-item-remove-btn verdana" data-remove-item="${item.id}">remove</button>
+    <button class="checkout-item-remove-btn verdana" data-remove-item="">remove</button>
     <p class="checkout-item-price">$${item.price}</p>
     </div>
     `    
@@ -47,8 +55,8 @@ function renderCheckoutItemHtml(item) {
 function renderCheckoutTotalPrice(item) {
     orderPriceArray.push(item.price)
 
-    orderTotal = orderPriceArray.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue
+    orderTotal = orderPriceArray.reduce((total, currentValue) => {
+        return total + currentValue
     })
     
     document.getElementById("checkout-total-price").innerText = `$${orderTotal}`
