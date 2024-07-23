43668ae0f11fc6ad0b743b57745de81612e093c1
taylornorris <taylornorrisdev@gmail.com>
2024-07-23 09:45:17 -0600

    Work on form logic
    It's pretty broken right now. In the middle of some refactoring and optimizing.

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # scrimba_mobile_restaurant_menu
 Scrimba Solo Project 7 - Mobile Restaurant Menu
 
-
-Additional Goals
-Modal rendered with JS
-Show order total on pay button
-Go back to edit order
+Add:
+when order complete, hide menu and show order
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import { menuArray } from "./data.js"
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
 
-const waitingForOrder = document.getElementById("waiting-for-order")
-const checkout = document.getElementById("order-section")
-
-
 let orderArray = []
 let orderStarted = false
 
-// LISTEN FOR CLICKS AND CALL FUNCTIONS
+const orderBtn = document.getElementById("order-btn")
+const modalCloseBtn = document.getElementById("modal-close-btn")
+
+// EVENT LISTENERS
 document.addEventListener("click", function(e) {
     if (e.target.dataset.addItem) {
         addItemToOrder(menuArray[e.target.dataset.addItem]) 
@@ -16,10 +15,23 @@ document.addEventListener("click", function(e) {
     if (e.target.dataset.removeItem) {
         removeItemFromOrder(e.target.dataset.removeItem)
     }
-    if (e.target.dataset.orderBtn) {
-        document.getElementById("modal").classList.toggle("hidden")
+    // if (e.target.dataset.modalCloseBtn) {
+    //     document.getElementById("modal").classList.toggle("hidden")
+    // }
+    if (e.target.dataset.payBtn) {
+        const formData = new FormData(document.getElementById("modal"))
+        const name = formData.get("name")
+        console.log(name)
     }
 })
+
+orderBtn.addEventListener("click", function() {
+    renderModal()
+})
+
+// modalCloseBtn.addEventListener("click", function() {
+//     document.getElementById("modal").classList.toggle("hidden")
+// })
 
 // ADD ITEM TO ORDER
 function addItemToOrder(item) {
@@ -37,11 +49,11 @@ function removeItemFromOrder(value) {
     handleOrderState()
 }
 
-// SHOW / HIDE ORDER DEPENDING ON ORDER STATE
+// SHOW OR HIDE ORDER DEPENDING ON ORDER STATE
 function handleOrderState() {
     if (orderArray.length >= 1) {
-        orderStarted = true
         renderOrderHtml()
+        orderStarted = true
     }
     else orderStarted = false
     
@@ -87,7 +99,61 @@ function getOrderTotalPrice() {
     return orderTotalPrice
 }
 
-// RENDER MENU HTML
+// RENDER MODAL
+function renderModal() {
+    document.getElementById("modal").classList.toggle("hidden")
+    document.getElementById("modal").innerHTML = `
+    <div class="modal-div radius">
+        <button id="modal-close-btn" class="modal-close-btn">x</button>
+        <h4 class="verdana">Enter card details</h4>
+        <div class="input-div">
+            <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+                pattern="[a-zA-Z ]{2,}"
+                class="verdana radius"
+                required
+            >
+            <input
+                type="text"
+                id="card-number"
+                name="card-number"
+                minlength="16"
+                maxlength="16"
+                placeholder="Enter card number"
+                pattern="[0-9]{16}"
+                class="verdana radius"
+                required
+            >
+            <input
+                type="text"
+                id="cvv-number"
+                name="cvv-number"
+                minlength="3"
+                maxlength="3"
+                placeholder="Enter CVV"
+                pattern="[0-9]{3}"
+                class="verdana radius"
+                required
+            >
+        </div>
+        <button 
+            type="submit" 
+            class="pay-btn verdana green-btn radius" 
+            data-pay-btn="1">
+            Pay $${getOrderTotalPrice()}
+        </button>
+    </div>
+    `
+}
+
+function completeOrder(name) {
+    document.getElementById("waiting-for-order").classList.toggle("hidden")
+}
+
+// RENDER MENU
 document.getElementById("menu-section").innerHTML = getMenuHtml()
 
 function getMenuHtml() {
